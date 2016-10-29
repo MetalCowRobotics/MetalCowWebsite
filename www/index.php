13c19a31e49dc786cfaa88116227bb7d1553caf4
Tim <wayfarer1313@gmail.com>
2016-10-28 20:21:22 -0500

    index comment fix
    I had the <!comment> outside the <php block

--- www/index.php
+++ www/index.php
@@ -1,3 +1,7 @@
+<?php
+
 //This is a total hack to make Heroku serve an HTML only site
 //this will be temp?  Until we get more content?
-<?php include_once("home.html"); ?>
+include_once("home.html");
+
+?>
