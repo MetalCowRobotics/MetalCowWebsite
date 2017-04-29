7e05569013e51874a6111fb4cafbd9999f7d08fb
Tim <wayfarer1313@gmail.com>
2017-04-29 18:01:11 -0500

    footer

---
+++ www/footer.php
@@ -0,0 +1,42 @@
+<footer id="footer" role="contentinfo">
+  <div class="footer-widget-wrap">
+    <div class="container">
+      <div class="footer-widget col-xs-12">
+        <aside id="text-11" class=
+        "widget footer-widget-footer widget_text">
+          <h1 class="widget-title">
+            A special thanks to our sponsors:
+          </h1>
+
+          <div class="textwidget">
+            <img src="img/MeyerEMS.png" alt="Meyer EMS" width='100'>
+            <img src="img/ABCNucor.png" alt="ABC Nucor" width='100'>
+            <img src="img/ACC1.png" alt="ACC Electronix" width='100'>
+            <img src="img/asgrow.png" alt="Asgrow" width='100'>
+            <img src="img/bridgestone.png" alt="Culligan" width='100'>
+            <img src="img/culligan.png" alt="Culligan" width='100'>
+            <img src="img/dekalb.png" alt="DeKalb" width='100'>
+            <img src="img/DuPont.png" alt="DuPont" width='100'>
+            <img src="img/firstFarmers.png" alt="First Farmers" width='100'>
+            <img src="img/g3.png" alt="G3 Machining" width='100'>
+            <img src="img/growmark.png" alt="Growmark" width='100'>
+            <img src="img/HPEnterprise.png" alt="HP Enterprise" width='100'>
+            <img src="img/kss.png" alt="KSS" width='100'>
+            <img src="img/microsoft1.png" alt="Microsoft" width='100'>
+            <img src="img/crossroadsCenter.png" alt="Crossroads Center" width='100'>
+            <img src="img/Pioneer.png" alt="Pioneer" width='100'>
+            <img src="img/StateFarm.png" alt="State Farm" width='100'>
+            <img src="img/timelessClovers.png" alt="4H Timeless Clovers" width='100'>
+            <img src="img/tripSheetCentral.png" alt="Tripsheet Central" width='100'>
+            <br /><br />
+            <small style="color:grey">Zerif Lite theme elements by
+            Themeisle | select icons designed by Freepik and
+            distributed by Flaticon | photos by MetalCow
+            photographers (v1.0)</small>
+          </div>
+        </aside>
+      </div>
+    </div>
+  </div>
+  <div class="container"></div><!-- / END CONTAINER -->
+</footer><!-- / END FOOOTER  -->
