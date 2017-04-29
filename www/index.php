b9737c90cbcd39fbb4f1bbb9eebb760866e24369
Tim <wayfarer1313@gmail.com>
2017-04-29 17:37:13 -0500

    Home.php
    Setting up the index and home page.

--- www/index.php
+++ www/index.php
@@ -1,7 +1,72 @@
-<?php
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
 
-//This is a total hack to make Heroku serve an HTML only site
-//this will be temp?  Until we get more content?
-include_once("home.html");
+<!--[if lt IE 9]>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+<link rel="stylesheet" href="css/ie.css" type="text/css">
+<![endif]-->
 
-?>
+
+    <title>
+      MetalCow Robotics | Part Sport, Part Technology - 100% Awesome!
+    </title>
+
+    <style type="text/css">
+		.footer-widget {
+            float: none;
+            display: inline-block;
+            vertical-align: top;
+            width: 100%;
+        }
+        .footer-widget img{
+        	/*width:75px;*/
+            padding-top:25px;
+            padding-left:10px;
+            padding-right:10px;
+        }
+		body.custom-background {
+			background-image: url('img/background-20160504-a.jpg');
+			background-repeat: no-repeat;
+			background-position: top left;
+			background-attachment: scroll;
+	  	}
+
+		.header-content-wrap {
+			padding-top: 210px !important;
+		}
+    </style>
+
+    <!--<link rel='stylesheet' id='zerif_font-css' href=
+    '//fonts.googleapis.com/css?family=Lato%3A300%2C400%2C700%2C400italic%7CMontserrat%3A700%7CHomemade+Apple&amp;subset=latin%2Clatin-ext'
+    type='text/css' media='all'>
+    <link rel='stylesheet' id='zerif_font_all-css' href=
+    '//fonts.googleapis.com/css?family=Open+Sans%3A400%2C300%2C300italic%2C400italic%2C600italic%2C600%2C700%2C700italic%2C800%2C800italic&amp;ver=4.3.1'
+    type='text/css' media='all'>-->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
+    <link rel='stylesheet' id='zerif_style-css' href='css/style.css' type='text/css' media='all'>
+    <link rel='stylesheet' id='zerif_responsive_style-css' href='css/responsive.css' type='text/css' media='all'>
+    <link rel="icon" href="img/cropped-logo4-32x32.png" sizes="32x32">
+    <link rel="icon" href="img/cropped-logo4-192x192.png" sizes="192x192">
+    <link rel="apple-touch-icon-precomposed" href="img/cropped-logo4-180x180.png">
+    <meta name="msapplication-TileImage" content="img/cropped-logo4-270x270.png">
+  </head>
+
+
+  <?php
+  //Page routing goes here, should be based on the URL Params
+
+
+
+  include_once("home.html");
+
+
+
+  ?>
+
+</html>
